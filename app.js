765172bb215d0d4e2f76e14d83ad76d1bbe48c18
Dhaval Kapil <dhavalkapil@gmail.com>
2014-12-04 15:22:14 +0530

    Ready for publish - added bin/

--- app.js
+++ app.js
@@ -9,33 +9,37 @@ var ui = require('./lib/ui.js');
 var log = require('./lib/log.js');
 
 // Starting a capture session
-var pcap_session = pcap.createSession(args.params.interface, args.params.filter);
+var start = function() {
+	var pcap_session = pcap.createSession(args.params.interface, args.params.filter);
 
-// Creating a TCP tracker
-var tcp_tracker = new pcap.TCP_tracker();
+	// Creating a TCP tracker
+	var tcp_tracker = new pcap.TCP_tracker();
 
-tcp_tracker.on('http request', function(session, http) {
-	var logString = http.request.method + ' ' + http.request.url + ' HTTP/' + http.request.http_version;
-	var headers = http.request.headers;
+	tcp_tracker.on('http request', function(session, http) {
+		var logString = http.request.method + ' ' + http.request.url + ' HTTP/' + http.request.http_version;
+		var headers = http.request.headers;
 
-	var timestamp = moment();
+		var timestamp = moment();
 
-	// display log entry in ui
-	ui.displayLog(logString, headers, timestamp);
+		// display log entry in ui
+		ui.displayLog(logString, headers, timestamp);
 
-	// Log the packet transfer to a file if specified
-	if(typeof args.params.log !== 'undefined') {
-		log.writeLog(http.request, timestamp, args.params.log);
-	}
-});
+		// Log the packet transfer to a file if specified
+		if(typeof args.params.log !== 'undefined') {
+			log.writeLog(http.request, timestamp, args.params.log);
+		}
+	});
 
-// Listening on packets
-pcap_session.on('packet', function(raw_packet) {
-	// Decoding raw packet
-	var packet = pcap.decode.packet(raw_packet);
+	// Listening on packets
+	pcap_session.on('packet', function(raw_packet) {
+		// Decoding raw packet
+		var packet = pcap.decode.packet(raw_packet);
 
-	// Track TCP packets
-	if(packet.link.ip.protocol_name === 'TCP') {
-		tcp_tracker.track_packet(packet);
-	}
-});
+		// Track TCP packets
+		if(packet.link.ip.protocol_name === 'TCP') {
+			tcp_tracker.track_packet(packet);
+		}
+	});
+};
+
+exports.start = start;
